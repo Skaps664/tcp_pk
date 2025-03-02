71a41b8e65d68b79720a3a797400b931f6bb4f9a
Skaps664 <msudaisk664@gmail.com>
2025-03-02 06:15:22 +0500

    machine change

---
+++ src/app/(admin)/studio/layout.tsx
@@ -0,0 +1,13 @@
+import "../globals.css";
+
+import React from 'react'
+
+const RootLayout = ({children}: {children: React.ReactNode} ) => {
+  return (
+    <html lang='en'>
+        <div>RootLayout</div>
+    </html>
+  )
+}
+
+export default RootLayout
--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,6 +1,4 @@
 import Container from "@/components/Container";
-import Header from "@/components/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
